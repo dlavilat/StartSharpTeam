881590ee8d09e2cad8adc363332fb3a7a2b75cce
dlavilat <48604347+dlavilat@users.noreply.github.com>
2022-02-04 10:51:24 -0500

    update MainMenu - userinterface - MENU_NORTHWIND

--- src/main/java/userinterface/MainMenu.java
+++ src/main/java/userinterface/MainMenu.java
@@ -5,6 +5,7 @@ import net.serenitybdd.screenplay.targets.Target;
 
 public class MainMenu {
 
-    public static final Target MENU_NORTHWIND = Target.the("Menú NorthWind").located(By.xpath("//span[contains(text(),'Northwind')]"));
+    public static final Target MENU_NORTHWIND = Target.the("Menú NorthWind")
+            .located(By.xpath("//aside[@id=\"s-sidebar\"]//descendant::span[contains(text(),'Northwind')]"));
     public static final Target MENU_ORDERS = Target.the("Menú Orders").located(By.xpath("//span[contains(text(),'Orders')]"));
 }
